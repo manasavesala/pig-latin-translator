05adfc7a78a3421582139a19af970021dbfc6a0a
manasa vesala and stuart gill <stuart@>
2018-10-24 15:47:05 -0700

    pig latin max code

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,45 @@
 var sentenceScrambler = function(sentence) {
-  var characterSplit = sentence.split(" ");
-  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-  // var consonants = ["b" etc];
-  for (x=0; x < characterSplit.length; x++) {
 
-    if (vowels.includes(characterSplit[x])) {
-      characterSplit[x] = characterSplit[x] + "ay ";
-      }
-    else if (characterSplit[x].length>1){
-      var wordArray = characterSplit[x].split("");
-      var letter = /^[a-zA-Z]+$/;
-      if(wordArray[0].match(letter)){
-        if(vowels.includes(wordArray[0])){
-          wordArray[wordArray.length-1]=wordArray[wordArray.length-1]+"way";
-        }
-        // could these nested ifs below become one for loop?
-        if(!vowels.includes(wordArray[0])){
-          if(!vowels.includes(wordArray[1])){
-            if(!vowels.includes(wordArray[2])){
-              wordArray[wordArray.length]=wordArray[0]+wordArray[1]+wordArray[2]+"ay";
-              wordArray[0]="";
-              wordArray[1]="";
-              wordArray[2]="";
-            }
-            else if ((wordArray[1]===("q"||"Q"))&&(wordArray[2]===("u"||"U"))){
-                wordArray[wordArray.length]=wordArray[0]+wordArray[1]+wordArray[2]+"ay";
-                wordArray[0]="";
-                wordArray[1]="";
-                wordArray[2]="";
-              }
-            else{
-              wordArray[wordArray.length]=wordArray[0]+wordArray[1]+"ay";
-              wordArray[0]="";
-              wordArray[1]="";
-            }
-          }
-          else{
-            if((wordArray[0]===("q"||"Q"))&&(wordArray[1]===("u"||"U"))){
-              wordArray[wordArray.length]=wordArray[0]+wordArray[1]+"ay";
-              wordArray[0]="";
-              wordArray[1]="";
-            }
-            else{
-              wordArray[wordArray.length]=wordArray[0]+"ay";
-              wordArray[0]="";
-            }
+  var splitSentence = sentence.split(" ");
+  var mappedLatin = splitSentence.map(function(word) {
 
-          }
-        }
-      }
-      characterSplit[x]=(wordArray.join(""));
+    if (word.slice(0, 2).toLowerCase() == "qu") {
+      return word.slice(2, word.length) + word.slice(0, 2) + "ay"
     }
-  }
-  return characterSplit.join(" ");
+
+    if ("AEIOUaeiou".includes(word[0]) && word.length == 1) {
+      return word + "ay";
+    }
+
+    if ("AEIOUaeiou".includes(word[0])) {
+      return word + "way";
+    }
+
+    if (!"AEIOUaeiou".includes(word[0])) {
+      for (x = 0; x < word.length; x++) {
+        if ("AEIOUaeiou".includes(word[x])) {
+          var vowel = x
+          return word.slice(x, word.length) + word.slice(0, x) + "ay";
+        }
+      }
+    }
+});
+  return mappedLatin.join(" ");
 }
+
+
+
+
+
+
+    // var letter = /^[a-zA-Z]+$/;
+    // if(word[0].match(letter)){
+
 
 $(document).ready(function() {
   $("button").click(function(){
     var inputSentence = $("#inputSentence").val();
-    var scrambledSentence = sentenceScrambler(inputSentence);
-    $("#output").text("Your coded sentence is: " + scrambledSentence);
+    $("#output").text(sentenceScrambler(inputSentence));
 
 
     });
